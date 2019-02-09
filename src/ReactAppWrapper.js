0b247fdb897ef01c9c37fe40421c01db9bb6e98f
Mihai Avram <mihai.v.avram@gmail.com>
2019-02-08 21:55:28 -0600

    Adding website to barebones react version (non-working still).

---
+++ src/ReactAppWrapper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './ReactAppWrapper.css';
+
+class ReactAppWrapper extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ReactAppWrapper;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+    Created on : 08/19/2018
+    Author     : Mihai Avram
+    E-Mail: mihai.v.avram@gmail.com
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ReactAppWrapper from './ReactAppWrapper';
+
+ReactDOM.render(<ReactAppWrapper />, document.getElementById('root'));
