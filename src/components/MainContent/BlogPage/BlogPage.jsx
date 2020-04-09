3f66396402231bfe050f7e3b804dfd208b6a9d96
Mihai Avram <mihai.v.avram@gmail.com>
2020-04-09 17:07:51 -0500

    Adding new blog post about no code as of April.

--- src/components/MainContent/BlogPage/BlogPage.jsx
+++ src/components/MainContent/BlogPage/BlogPage.jsx
@@ -16,6 +16,26 @@ class BlogPage extends Component {
             <b>
             <a
               target="_blanks"
+              href="https://mihaisplace.blog/2020/04/09/a-brief-summary-of-no-code-and-the-main-players-in-the-space/"
+              rel="noopener noreferrer">
+              A Brief Summary of "No Code" and the Main Players in the Space
+            </a>
+            </b>
+            </div>
+            <div className="small-text">
+              <i>4/9/2020</i>
+            </div>
+            <div className="small-text">
+              <u>Tags:</u> NoCode, LowCode, Code Faster, Build Faster, Agile
+            </div>
+          </div>
+        </div>
+        <div className='blog-template'>
+          <div>
+            <div>
+            <b>
+            <a
+              target="_blanks"
               href="https://mihaisplace.blog/2020/02/17/tools-and-techniques-to-help-you-code-and-work-faster/"
               rel="noopener noreferrer">
               Tools and Techniques to Help You Code and Work Faster
